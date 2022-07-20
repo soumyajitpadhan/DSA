3de5e5e782c253fb180e34eb19bdbbf90abdb410
Soumyajit Padhan <108894962+soumyajitpadhan@users.noreply.github.com>
2022-07-20 16:39:47 +0530

    Create insertion_sort.cpp

---
+++ Sorting/insertion_sort.cpp
@@ -0,0 +1,19 @@
+void insertionSort(int n, vector<int> &arr){
+    int i = 1;
+    while(i < n) {
+        int temp = arr[i];
+        
+        int j = i - 1;
+        while(j >= 0) {
+            if(arr[j] > temp) {
+                arr[j+1] = arr[j];
+            }
+            else {
+                break;
+            }
+            j--;
+        }
+        arr[j+1] = temp;
+        i++;
+    }
+}
