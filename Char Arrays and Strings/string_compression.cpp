fd9296e5ace4030011c1f9ecb78dcea66ef955c5
Soumyajit Padhan <108894962+soumyajitpadhan@users.noreply.github.com>
2022-08-10 16:33:09 +0530

    Create string_compression.cpp

---
+++ Char Arrays and Strings/string_compression.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+        int i = 0;
+        int ansIndex = 0;
+        int n = chars.size();
+        
+        while(i<n)
+        {
+            int j = i+1;
+            while(j<n && chars[i]==chars[j])
+            {
+                j++;
+            }
+            //yaha kab aaoge
+            //ya toh vector poora traverse krdia
+            //ya fer new/Different character encounter kia hai
+            
+            
+            //oldChar store karlo
+            chars[ansIndex++] = chars[i];
+            
+            int count = j-i;
+            
+            if(count > 1) {
+                //converting counting into single digit and saving in answer
+                string cnt = to_string(count);
+                for(char ch: cnt)
+                {
+                    chars[ansIndex++] = ch;
+                }
+            }
+            //moving to new/Different character
+            i=j;
+        }
+        return ansIndex;
+    }
+};
+
+//T.C = O(n) , S.C = O(1)
